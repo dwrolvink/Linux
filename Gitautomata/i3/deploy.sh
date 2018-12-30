6a25c6712dd089d056b721a9800dc36746b1dd9f
Dorus <30291690+dwrolvink@users.noreply.github.com>
2018-12-30 23:24:42 +0100

    Create deploy.sh

---
+++ Gitautomata/i3/deploy.sh
@@ -0,0 +1,22 @@
+#!bin/sh
+
+# Update
+pacman -Syuu
+
+# Install applications
+pacman -S chromium
+pacman -S pcmanfm
+pacman -S smbclient
+
+# Terminal settings
+unalias cp
+
+# Copy files over
+cp -f config /home/dorus/.i3/config
+cp -f mountnwd.service /etc/systemd/system/mountnwd.service
+
+# Mount network drive
+cp -f mountnwd.service /etc/systemd/system/mountnwd.service
+systemctl enable /etc/systemd/system/mountnwd.service
+systemctl start mountnwd    #start directly for testing
+
