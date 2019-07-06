f6d43b5ac764aa82c3ea540c99497bcb4375f2dc
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-07-06 16:58:15 +0200

    Create server_configuration.sh

---
+++ Manjaro/server_configuration.sh
@@ -0,0 +1,4 @@
+# This script is meant to configure standard manjaro servers (as opposed to a manjaro box used as a workstation)
+
+# Fixes "Error opening terminal: rxvt-unicode-256color." error over SSH
+sudo pacman -Sy rxvt-unicode-terminfo
