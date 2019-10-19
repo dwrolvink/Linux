4719e05af6cba2abf5e5a13da009399580a4fa04
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-10-19 17:09:03 +0200

    Update echo_v2.asm

--- assembly_x86_64/examples/echo_v2.asm
+++ assembly_x86_64/examples/echo_v2.asm
@@ -1,32 +1,31 @@
 ; Code written for: Linux x86_64
 ; Tutorial: https://www.youtube.com/playlist?list=PLetF-YjXm-sCH6FrTz4AQhfH6INDQvQSn
 
-
 section .bss
     name resb 16  ; reserve 16 bytes for $name
     
 section .data
 	; script data
-    text_query db "What is your name?: ",0
-    text_hello db "Hello ",0
+	text_query db "What is your name?: ",0
+	text_hello db "Hello ",0
 
 	; lib data
 	ClearTerm db   27,"[H",27,"[2J",0    ; <ESC> [H <ESC> [2J ; needed for cls()
     
 section .text
-    global _start
+	global _start
     
 _start:
 	call cls				; clearscreen
-    mov rax, text_query 	; ask for name
+	mov rax, text_query 	; ask for name
 	call print
 
-    call _getName			; readln
+	call _getName			; readln
 	call cls				; clearscreen
 	
-    mov rax, text_hello		; print hello
+	mov rax, text_hello		; print hello
 	call print
-    mov rax, name			; print name
+	mov rax, name			; print name
 	call print
 
 	call exit				; exit program
@@ -34,27 +33,27 @@ _start:
 ; script subroutines 
 ; ----------------------------------------------
 _getName:
-    mov rsi, name   ; store input into $name
-    mov rdx, 16     ; number of bytes to read in
-    call _read
-    ret
+	mov rsi, name   ; store input into $name
+	mov rdx, 16     ; number of bytes to read in
+	call _read
+	ret
 
 ; standard subroutines
 ; ----------------------------------------------
 
 ; exit program
 exit:
-    MOV RAX, 60
-    MOV RDI, 0
-    syscall    
+	MOV RAX, 60
+	MOV RDI, 0
+	syscall    
 
 ; sys_read
 ; call by: - put .bss buffer into rsi; - put length into rdx
 _read:
-    mov rax, 0      ; sys_read
-    mov rdi, 0      ; standard_input
-    syscall
-    ret
+	mov rax, 0      ; sys_read
+	mov rdi, 0      ; standard_input
+	syscall
+	ret
 
 ; prints string to standard_output
 ; call by putting a string into rax
