256f09595197a9051a18bae20c2d2669a829bc35
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-10-26 16:58:42 +0200

    Create main.s

---
+++ assembly_x86_64/examples/include/main.s
@@ -0,0 +1,78 @@
+section .bss
+    name resb 16  ; reserve 16 bytes for $name
+    
+section .data
+	; script data
+	text_query db "What is your name?: ",0
+	text_hello db "Hello ",0
+
+	; lib data
+	ClearTerm db   27,"[H",27,"[2J",0    ; <ESC> [H <ESC> [2J ; needed for cls()
+    
+section .text
+	global _start
+    
+_start:
+	call cls		; clearscreen
+	mov rax, text_query	; ask for name
+	call print
+
+	call _getName		; readln
+	call cls		; clearscreen
+	
+	mov rax, text_hello	; print hello
+	call print
+	mov rax, name		; print name
+	call print
+
+	call exit		; exit program
+
+; script subroutines 
+; ----------------------------------------------
+_getName:
+	mov rsi, name   ; store input into $name
+	mov rdx, 16     ; number of bytes to read in
+	call _read
+	ret
+
+; standard subroutines
+; ----------------------------------------------
+
+; exit program
+exit:
+	MOV RAX, 60
+	MOV RDI, 0
+	syscall    
+
+; sys_read
+; call by: - put .bss buffer into rsi; - put length into rdx
+_read:
+	mov rax, 0      ; sys_read
+	mov rdi, 0      ; standard_input
+	syscall
+	ret
+
+; prints string to standard_output
+; call by putting a string into rax
+print:
+	push rax 		; push string ref to the stack
+	mov rbx, 0		; $count = 0
+__print_loop:
+	inc rax			; $i++, $i-> $string[$i]
+	inc rbx			; $count++
+	mov cl, [rax]		; move 8 bit (one ASCII char) into cl
+	cmp cl, 0		; (char == $null)
+	jne __print_loop	; else
+
+	mov rax, 1		; sys_write
+	mov rdi, 1   		; standard_output
+	pop rsi 		; pop string ref to rsi
+	mov rdx, rbx		; put count in place for syscall
+	syscall
+	ret
+
+; Clear screen
+cls:
+	mov rax, ClearTerm
+	call print
+	ret
